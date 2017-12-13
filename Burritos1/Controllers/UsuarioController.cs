1aaf9c9e965ee0be459b8e2b850a2d09a4d30484
marcelcelaya <marcel.celaya@uabc.edu.mx>
2017-12-12 23:24:29 -0800

    Solucion a bug que una vez de solicitar un producto, se mostraba datos en blanco

--- Burritos1/Controllers/UsuarioController.cs
+++ Burritos1/Controllers/UsuarioController.cs
@@ -66,7 +66,7 @@ namespace Burritos1.Controllers
                 TempData["Message"] = "Exito";
                 return View(mimodelo);
             }
-            return View();
+            return RedirectToAction("BurritosEnVenta");
         }
         public ActionResult MisOrdenes()
         {
